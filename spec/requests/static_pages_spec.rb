2eda317e76c1ede7c89a8eb97c381e7af904b5cf
Adriano Gaved <agaved@gmail.com>
2012-10-14 17:29:10 -0300

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,20 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+let(:title_preamble) {"Ruby on Rails Tutorial Sample App"}
 
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => "Sample App")
     end
-    it "should have the title 'Home'" do
+    it "should have the the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+    end
+    it "should not have the the base title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -20,7 +25,7 @@ describe "StaticPages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title', :text => "#{title_preamble} | Help")
     end
   end
 
@@ -31,7 +36,7 @@ describe "StaticPages" do
     end
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_selector('title', :text => "#{title_preamble} | About Us")
     end
   end
 
@@ -42,7 +47,7 @@ describe "StaticPages" do
     end
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+      page.should have_selector('title', :text => "#{title_preamble} | Contact")
     end
   end
 
